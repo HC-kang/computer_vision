1b62981fb10b88a5086bf5232fcf450bf50abbda
HC-kang <weston0713@gmail.com>
2021-06-30 23:27:21 +0900

    0630

--- 0630_alexnet_homework.py
+++ 0630_alexnet_homework.py
@@ -11,8 +11,8 @@ import time
 y_train.shape
 
 num_classes = 10
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
 
 y_train.shape
 
@@ -25,22 +25,42 @@ model = keras.models.Sequential([
                                  
                                  
     keras.layers.Conv2D(filters=96, kernel_size=(3,3), strides=(1,1), activation='relu', input_shape=(32,32,3)),
+    # 30,30,96
     keras.layers.BatchNormalization(),
+    # 30,30,96
     keras.layers.MaxPool2D(pool_size=(3,3), strides=(4 ,4 )),
-    keras.layers.Conv2D(  , padding="same"),
+    # 7,7,96
+    keras.layers.Conv2D(256, kernel_size = (5,5), padding="same"),
+    # 7,7,256
     keras.layers.BatchNormalization(),
-    keras.layers.MaxPool2D(  ),
-    keras.layers.Conv2D(  , padding="same"),
+    # 7,7,256
+    keras.layers.MaxPool2D(pool_size = (1,1), strides = (3,3)), #TODO:  1. 3,2   2. 2,2   3. 1,3
+    # 3,3,256
+    keras.layers.Conv2D(384, kernel_size = (3,3) , padding="same"),
+    # 3,3,384
     keras.layers.BatchNormalization(),
-    keras.layers.Conv2D(  , padding="same"),
+    # 3,3,384
+    keras.layers.Conv2D(384, kernel_size = (1,1), padding="same"),
+    # 3,3,384
     keras.layers.BatchNormalization(),
-    keras.layers.Conv2D(  , padding="same"),
+    # 3,3,384
+    keras.layers.Conv2D(256, kernel_size = (1,1), padding="same"),
+    # 3,3,256
     keras.layers.BatchNormalization(),
-    keras.layers.MaxPool2D( ),
+    # 3,3,256
+    keras.layers.MaxPool2D(pool_size = (2,2)), #TODO:
+    # 1,1,256
     keras.layers.Flatten(),
-    keras.layers.Dense(  ),
+    # 256
+    keras.layers.Dense(4096),
+    # 4096
     keras.layers.Dropout(0.5),
-    keras.layers.Dense(  ),
+    # 4096
+    keras.layers.Dense(4096),
+    # 4096
     keras.layers.Dropout(0.5),
-    keras.layers.Dense(  , activation='softmax')
+    # 4096
+    keras.layers.Dense(10 , activation='softmax')
+    # 10
 ])
+model.summary()
